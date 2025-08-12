36aef7a4474d44865a19ef145d689360be2a4e0a
KristaUSingh <109554138+KristaUSingh@users.noreply.github.com>
2025-08-12 00:19:19 -0400

    added photo, update resume, change color scheme

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
+
+window.addEventListener('load', () => {
+    const avatar = document.querySelector('.content .avatar');
+    if (avatar) avatar.classList.add('is-visible');
+});
+
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
